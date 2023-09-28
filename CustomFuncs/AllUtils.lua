7a6dd069d782712c092c61bc98b3919ca29b7ce9
yMajesty <118635204+yMajestyx@users.noreply.github.com>
2023-09-28 15:45:44 -0300

    Update AllUtils.lua

--- CustomFuncs/AllUtils.lua
+++ CustomFuncs/AllUtils.lua
@@ -1,1 +1,9 @@
-
+local u_tils = {
+    Pathfinding = loadstring(game:HttpGet("https://raw.githubusercontent.com/yMajestyx/Project/main/CustomFuncs/PathfindingLib.lua"))(),
+    Network = loadstring(game:HttpGet("https://raw.githubusercontent.com/yMajestyx/Project/main/CustomFuncs/Network.lua"))(),
+    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/yMajestyx/Project/main/CustomFuncs/WebhookLib.lua"))(),
+    Suffix = loadstring(game:HttpGet("https://raw.githubusercontent.com/yMajestyx/Project/main/CustomFuncs/SuffixLib.lua"))(),
+    Table = loadstring(game:HttpGet("https://raw.githubusercontent.com/yMajestyx/Project/main/CustomFuncs/TableLib.lua"))(),
+    Esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/yMajestyx/Project/main/CustomFuncs/EspLib.lua"))()
+}
+return u_tils
